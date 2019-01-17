e40fb0cee6230f8fb62aefc37b92b380e3b138da
peefer66 <peefer1966@gmail.com>
2019-01-17 20:52:37 +0000

    Register.html

--- author/forms_author.py
+++ author/forms_author.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import Validators, StringField, PasswordField
+from wtforms import validators, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
 
 class RegisterForm(FlaskForm):
-    full_name = StringFiield('Full Name', [validators.InputRequired()])
+    full_name = StringField('Full Name', [validators.InputRequired()])
     email = EmailField('Email address', [validators.InputRequired(), validators.Email()])
     password = PasswordField('New Password',[
         validators.InputRequired(),
         validators.Length(min=4, max=80)
     ])
-    confirm = PasswordField('Repeat Password', [validators.Equalto(message='Passwords must match')
+    confirm = PasswordField('Repeat Password', [validators.EqualTo(password,message='Passwords must match')
     ])
