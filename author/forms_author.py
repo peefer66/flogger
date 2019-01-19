67728a2f5e1ae8211f9e026639791bff4e1b5868
peefer66 <peefer1966@gmail.com>
2019-01-19 12:51:25 +0000

    6/68 validate user password

--- author/forms_author.py
+++ author/forms_author.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import validators, StringField, PasswordField, ValidationError
 from wtforms.fields.html5 import EmailField
+from werkzeug.security import check_password_hash
 
 from author.models_author import Author
 
@@ -22,3 +23,21 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = EmailField('Email Adress', [validators.InputRequired(),validators.Email()])
     password = PasswordField('Password', [validators.InputRequired(), validators.Length(min=4, max=80)])
+
+    # validate the user and passowrd with the database
+    def validate(self):
+        rv = FlaskForm.validate(self)
+        if not rv:
+            return False
+        # Create instance of Author and query on email
+        author = Author.query.filter_by(email=self.email.data).first()
+        # If email exists in database 
+        if author:
+            # If password incorrect 
+            if not check_password_hash(author.password, self.password.data):# Compare password with hashed password
+                self.password.errors.append('Incorrect email or password') # always error both the email and password!!
+                return False
+            return True
+        # email doesn't existin database
+        else:
+            return 'Incorrect email or password'
