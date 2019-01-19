934d145656a7867047146bdc7ee543c16da91ace
peefer66 <peefer1966@gmail.com>
2019-01-19 12:20:59 +0000

    6/67 Author login

--- author/forms_author.py
+++ author/forms_author.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import validators, StringField, PasswordField
+from wtforms import validators, StringField, PasswordField, ValidationError
 from wtforms.fields.html5 import EmailField
+
+from author.models_author import Author
 
 class RegisterForm(FlaskForm):
     full_name = StringField('Full Name', [validators.InputRequired()])
@@ -11,3 +13,12 @@ class RegisterForm(FlaskForm):
     ])
     confirm = PasswordField('Repeat Password', [validators.EqualTo('password',message='Passwords must match')
     ])
+
+    def validate_email(self, email):
+        author = Author.query.filter_by(email=email.data).first()
+        if author is not None:
+            raise ValidationError('Email already in use')
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email Adress', [validators.InputRequired(),validators.Email()])
+    password = PasswordField('Password', [validators.InputRequired(), validators.Length(min=4, max=80)])
--- author/views_author.py
+++ author/views_author.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from werkzeug.security import generate_password_hash
 
 from author.models_author import Author
-from author.forms_author import RegisterForm
+from author.forms_author import RegisterForm, LoginForm
 from application import db
 
 author_app = Blueprint('author_app', __name__)
@@ -13,7 +13,7 @@ def register():
     # If the user input is valid
     if form.validate_on_submit():
         # Python debugger
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
         # Generate an hashed password
         hashed_password = generate_password_hash(form.password.data)
         #Create an instance of the Authot
@@ -25,7 +25,17 @@ def register():
         #Save to database
         db.session.add(author)
         db.session.commit()
-
+        # Print to web
         return f'Author ID: {author.id}'
-        
+    # If not validate re-load register page 
     return render_template('author/register.html', form=form)
+
+@author_app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    error = None
+
+    if form.validate_on_submit():
+        return 'User Logged In'
+    return render_template('author/login.html', form=form, error=error)
+
