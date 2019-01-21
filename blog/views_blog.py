a670035d455237408f479d59fd0ce8e21841c237
peefer66 <peefer1966@gmail.com>
2019-01-21 22:09:16 +0000

    6/78 post

--- blog/views_blog.py
+++ blog/views_blog.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, session, render_template
+from slugify import slugify
 
 from blog.forms_blog import PostForm
 from blog.models_blog import Post, Category
 from author.models_author import Author
+from application import db
 
 blog_app =Blueprint('blog_app', __name__)
 
@@ -13,4 +15,40 @@ def index():
 @blog_app.route('/post', methods=('GET', 'POST'))
 def post():
     form = PostForm()
+    # Check for new category
+    if form.validate_on_submit():
+    # New category 
+        if form.new_category.data:
+    # Create an instance of the catagory with the description on it
+            new_category = Category(form.new_category.data)
+    # Add to a new session
+            db.session.add(new_category)
+    # Generate a category id without saving to DB.
+    # flush is like a pre-save allowing a genertion of a new id
+    # and keeping the session open for mods without saving to DB
+            db.session.flush()
+    # assign the new category
+            category = new_category
+    #If there is no new category assign to whatever is on the drop down
+        else:
+            category = form.category.data
+
+        # Data base opperations
+        author = Author.query.get(session['id']) # This assumes a user is already logged 
+        title = form.title.data.strip() # Strip leading and tailing blank
+        body = form.body.data.strip()
+        # Create a Post sqlalchemy object
+        post = Post(author=author, title=title,
+         body=body, category=category)
+        # save to database
+        db.session.add(post)
+        db.session.commit()
+
+        # generate a url from slugify
+        slug = slugify(str(post.id) + '-' + post.title)
+        post.slug = slug
+        db.session.commit()
+
+
+
     return render_template('blog/post.html', form=form)
